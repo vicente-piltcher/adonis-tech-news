eeb2d7d75eb7175274d957f658efbcce62d991bd
Vicente Piltcher <vipiltcher62@gmail.com>
2023-05-12 17:51:32 -0300

    a

--- start/routes.ts
+++ start/routes.ts
@@ -21,5 +21,9 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
-  return { hello: 'world' }
+  return {hello: 'Hello World!'}
+})
+
+Route.get('/addnews', async() => {
+
 })
